f39e5843dc98eb527e472cd46c4777e46f930624
Goran Miskovic <goran.miskovic@arbor-education.com>
2013-09-09 00:01:26 +0200

    instaling node.js

--- puppet/modules/socket.io/manifests/init.pp
+++ puppet/modules/socket.io/manifests/init.pp
@@ -1,3 +1,30 @@
+stage { 'first':
+  before => Stage['main'],
+}
+
+stage { 'last': }
+
+Stage['main'] -> Stage['last']
+
 node 'socketio.schkovich.local' {
-  include ssh
+
+  class {'ssh':
+    stage => first,
+  }
+
+
+  class { 'setcaps':
+    stage => first
+  }
+
+  class {'nodejs':
+    stage => first,
+  }
+  ->
+  exec { "set_capability":
+    # Open ports bellow 1024
+    command => "/sbin/setcap cap_net_bind_service=+ep /usr/bin/nodejs",
+    provider => shell,
+  }
+
 }
